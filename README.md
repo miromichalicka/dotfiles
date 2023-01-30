b36beeb92d5ba7ceecce8dd2ace149b8d722570c
Miro Michalicka <miro.michalicka@jobiqo.com>
2023-01-30 22:15:11 +0100

    First shot on modifications.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ My personal taste of localhost Ubuntu configuration. Tested on Ubuntu 22.04.
 Bootstrap on a fresh Ubuntu installation:
 
     sudo apt install git -y
-    git clone https://github.com/klausi/dotfiles.git
+    git clone https://github.com/miromichalicka/dotfiles.git
     cd dotfiles
 
 Execute with
--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ if ! which ansible > /dev/null; then
   sudo add-apt-repository universe -y
   sudo apt install ansible -y
   ansible-galaxy collection install community.general
+  ansible-galaxy install gantsign.oh-my-zsh
 fi
 
 # Run our playbook now.
-ansible-playbook ansible-setup.yml -i hosts --ask-become-pass
+ansible-playbook ansible-setup.yml -i hosts -K -v
